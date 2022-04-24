19e47a26e96c65466a3b15d9c44103ba01348168
robothot <854363956@qq.com>
2022-04-24 23:28:13 +0800

    feat: 添加 React 跨平台的桌面端开发脚手架支持

--- src/cli.ts
+++ src/cli.ts
@@ -4,13 +4,68 @@ import { prompt } from 'inquirer'
 import request from 'axios'
 import { Bar } from 'cli-progress'
 import { join } from 'path'
-import { existsSync, outputFile, readdir } from 'fs-extra'
+import { outputFile, readdir } from 'fs-extra'
 import JSZip from 'jszip'
 import { green, red } from 'ansi-colors'
 
 type InputType = {
-    projectType: 'normal' | 'ms-template' 
-    projectVersion: 'latest'
+    projectType: 'ms-native-app-template' | 'ms-template' 
+}
+
+async function downloadGitHub (projectName: string) {
+    const fileDownload = new Bar({
+        format: 'Working | {bar} | {percentage}% || {value}/{total}',
+        barCompleteChar: '\u2588',
+        barIncompleteChar: '\u2591',
+        hideCursor: true
+    });
+    
+    try {
+        const { data, headers } = await request.get(`https://codeload.github.com/Web-Lif/${projectName}/zip/refs/heads/canary`, {
+            responseType: "stream"
+        })
+
+        const totalLength = Number.parseInt(headers['content-length'])
+
+        fileDownload.start(totalLength, 0, {
+            task: 'download'
+        }) 
+
+        let bytes: any[] = []
+        let bytesLength: number = 0
+        data.on('data', (chunk: any) => {
+            bytes.push(chunk)
+            bytesLength += chunk.length
+            fileDownload.update(bytesLength)
+        })
+
+        data.on('end', () => {
+            fileDownload.stop()
+            const buf = Buffer.concat(bytes);
+            JSZip.loadAsync(buf).then((zip) => {
+                const files = zip.folder(projectName)?.files || {};
+                Object.keys(files).forEach(key => {
+                    const file = files[key]
+                    if (!file.dir) {
+                        files[key].async("uint8array").then((data) => {
+                            const fileDir = key.replace(`${projectName}-canary`, '')
+                            let path = join(process.cwd(), fileDir)
+                            if (process.env.TEST === 'true') {
+                                path = join(process.cwd(), 'tests', fileDir)
+                            }
+                            outputFile(path, data).then(() => {
+                                console.log(green(`+ ${fileDir}`))
+                            }).catch(err => {
+                                console.log(red(`E ${fileDir}\n - ${err.message}`))
+                            });
+                        })
+                    }
+                })
+            })
+        })
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 async function main () {
@@ -28,74 +83,11 @@ async function main () {
         message: "What type of project are you trying to create ?",
         choices: [
           'ms-template',
+          'ms-native-app-template',
         ],
-    }, {
-        type: 'list',
-        name: 'projectVersion',
-        message: "What version are you going to choose ?",
-        choices: [
-            'latest'
-        ]
     }])
 
-    // 下载最新的项目, 进行创建
-    if (result.projectType === 'ms-template' && result.projectVersion === 'latest') {
-        const fileDownload = new Bar({
-            format: 'Working | {bar} | {percentage}% || {value}/{total}',
-            barCompleteChar: '\u2588',
-            barIncompleteChar: '\u2591',
-            hideCursor: true
-        });
-        
-        try {
-            const { data, headers } = await request.get('https://codeload.github.com/Web-Lif/ms-template/zip/refs/heads/canary', {
-                responseType: "stream"
-            })
-
-            const totalLength = Number.parseInt(headers['content-length'])
-
-            fileDownload.start(totalLength, 0, {
-                task: 'download'
-            }) 
-
-            let bytes: any[] = []
-            let bytesLength: number = 0
-            data.on('data', (chunk: any) => {
-                bytes.push(chunk)
-                bytesLength += chunk.length
-                fileDownload.update(bytesLength)
-            })
-
-            data.on('end', () => {
-                fileDownload.stop()
-                const buf = Buffer.concat(bytes);
-                JSZip.loadAsync(buf).then((zip) => {
-                    const projectName = 'ms-template-canary'
-                    const files = zip.folder(projectName)?.files || {};
-                    Object.keys(files).forEach(key => {
-                        const file = files[key]
-                        if (!file.dir) {
-                            files[key].async("uint8array").then((data) => {
-                                const fileDir = key.replace(projectName, '')
-                                let path = join(process.cwd(), fileDir)
-                                if (process.env.TEST === 'true') {
-                                    path = join(process.cwd(), 'tests', fileDir)
-                                }
-                                outputFile(path, data).then(() => {
-                                    console.log(green(`+ ${fileDir}`))
-                                }).catch(err => {
-                                    console.log(red(`E ${fileDir}\n - ${err.message}`))
-                                });
-                            })
-                        }
-                    })
-                })
-            })
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
+    downloadGitHub(result.projectType)
 }
 
 main().catch((e) => {
